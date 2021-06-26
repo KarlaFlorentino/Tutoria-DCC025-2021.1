0cd9da037767067f41093ccdfcb595ad381cb2d3
KarlaFlorentino <karlaflorentino1999@gmail.com>
2021-06-25 22:17:40 -0300

    25.06.21

--- Exercicio04/src/main/java/com/mycompany/exercicio04/Main.java
+++ Exercicio04/src/main/java/com/mycompany/exercicio04/Main.java
@@ -18,59 +18,65 @@ public class Main {
         return experiencia += 1;
     }
     public static int locomover(char matriz[][], int ordem, int posicaoAtual[]){
-        int achouTesouro = 3; // 0 - não achou 1 - achou 3 - direção invalida
-        char opcao = JOptionPane.showInputDialog("w - cima; s - baixo; a - esquerda; d - direita. \n" + "Informe a dureção: ").charAt(0);
+        int achouTesouro = 0; 
+        char opcao = JOptionPane.showInputDialog("Digite:\n"
+                +                                "w - para mover o jogador para cima;\n"
+                +                                "s - para mover o jogador para baixo;\n"
+                +                                "a - para mover o jogador para esquerda;\n"
+                +                                "d - para mover o jogador para direita. \n" + 
+                                                 "Informe a direção: ").charAt(0);
         switch(opcao){
             case 'w':
                 if((posicaoAtual[0] - 1) >= 0){
-                    if( matriz[posicaoAtual[0] - 1][posicaoAtual[1]] == 'T') achouTesouro =  1;
-                    else achouTesouro =  0;
+                    if( matriz[posicaoAtual[0] - 1][posicaoAtual[1]] == 'T') achouTesouro = 1;
                     
                     matriz[posicaoAtual[0]][posicaoAtual[1]] = ' ';
                     matriz[posicaoAtual[0] - 1][posicaoAtual[1]] = 'J';
                     posicaoAtual[0] = posicaoAtual[0] - 1;
                 }else{
                     JOptionPane.showMessageDialog(null, "Direção inválida!");
+                    achouTesouro = -1;
                 }
                 break;
             case 's':
                 if((posicaoAtual[0] + 1) < ordem){
-                    if(matriz[posicaoAtual[0] + 1][posicaoAtual[1]] == 'T') achouTesouro =  1;
-                    else achouTesouro =  0;
+                    if(matriz[posicaoAtual[0] + 1][posicaoAtual[1]] == 'T') achouTesouro = 1;
                     
                     matriz[posicaoAtual[0]][posicaoAtual[1]] = ' ';
                     matriz[posicaoAtual[0] + 1][posicaoAtual[1]] = 'J';
                     posicaoAtual[0] = posicaoAtual[0] + 1;
                 }else{
                     JOptionPane.showMessageDialog(null, "Direção inválida!");
+                    achouTesouro = -1;
                 }
                 break;
             case 'a':
                 if((posicaoAtual[1] - 1) >= 0){
-                    if( matriz[posicaoAtual[0]][posicaoAtual[1] - 1] == 'T') achouTesouro =  1;
-                    else achouTesouro =  0;
+                    if( matriz[posicaoAtual[0]][posicaoAtual[1] - 1] == 'T') achouTesouro = 1;
                     
                     matriz[posicaoAtual[0]][posicaoAtual[1]] = ' ';
                     matriz[posicaoAtual[0]][posicaoAtual[1] - 1] = 'J';
                     posicaoAtual[1] = posicaoAtual[1] - 1;
                 }else{
                     JOptionPane.showMessageDialog(null, "Direção inválida!");
+                    achouTesouro = -1;
                 }
                 break;
             case 'd':
                 if((posicaoAtual[1] + 1) < ordem){
-                    if( matriz[posicaoAtual[0]][posicaoAtual[1] + 1] == 'T') achouTesouro =  1;
-                    else achouTesouro =  0;
+                    if( matriz[posicaoAtual[0]][posicaoAtual[1] + 1] == 'T') achouTesouro = 1;
                     
                     matriz[posicaoAtual[0]][posicaoAtual[1]] = ' ';
                     matriz[posicaoAtual[0]][posicaoAtual[1] + 1] = 'J';
                     posicaoAtual[1] = posicaoAtual[1] + 1;
                 }else{
                     JOptionPane.showMessageDialog(null, "Direção inválida!");
+                    achouTesouro = -1;
                 }
                 break;
             default:
                 JOptionPane.showMessageDialog(null, "Direção inválida!");
+                achouTesouro = -1;
         }   
         
         return achouTesouro;
@@ -97,19 +103,21 @@ public class Main {
             }
         }
         
-        /*for(int i = 0; i < ordem; i++){
-            for(int j = 0; j < ordem; j++){
-                System.out.print(matriz[i][j] + " | ");
-            }
-        
-            System.out.println("");
-        }
-        System.out.println("---------------");*/
-        
         int pontuacaoMax = experiencia + tesouros;
         int opcao = 0;
         while (experiencia != pontuacaoMax && opcao != 2){
-            opcao = Integer.parseInt(JOptionPane.showInputDialog("1 - Locomoção 2 - Sair \n" + "Informe a opção: "));
+            for(int i = 0; i < ordem; i++){
+                for(int j = 0; j < ordem; j++){
+                    System.out.print(matriz[i][j] + "|");
+                }
+                System.out.println("");
+            }
+            System.out.println("---------------");
+        
+            opcao = Integer.parseInt(JOptionPane.showInputDialog("Digite:\n "
+                    +                                            "1 - Para movimentar o Jogador\n "
+                    +                                            "2 - Para sair\n" + 
+                                                                 "Informe a opção: "));
             if(opcao == 1){
                 int aux = locomover(matriz, ordem, posicaoAtual);
                 
